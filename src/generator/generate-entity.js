1d461d836beac91143f1f1e16d1dd201142c4a01
slava <slava.milin@gmail.com>
2018-10-11 01:09:20 +0300

    add generate function and tests for it

---
+++ src/generator/generate-entity.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const AnnouncerSettings = {
+  MIN_X: 300,
+  MAX_X: 900,
+  MIN_Y: 150,
+  MAX_Y: 500,
+  MIN_PRICE: 1000,
+  MAX_PRICE: 1000000,
+  MIN_ROOMS: 1,
+  MAX_ROOMS: 5,
+  MIN_GUESTS: 1,
+  MAX_GUESTS: 10,
+  titles: [
+    `Большая уютная квартира`,
+    `Маленькая неуютная квартира`,
+    `Огромный прекрасный дворец`,
+    `Маленький ужасный дворец`,
+    `Красивый гостевой домик`,
+    `Некрасивый негостеприимный домик`,
+    `Уютное бунгало далеко от моря`,
+    `Неуютное бунгало по колено в воде`,
+  ],
+  types: [
+    `flat`,
+    `palace`,
+    `house`,
+    `bungalo`,
+  ],
+  checkins: [
+    `12:00`,
+    `13:00`,
+    `14:00`,
+  ],
+  checkouts: [
+    `12:00`,
+    `13:00`,
+    `14:00`,
+  ],
+};
+
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(2);
+};
+
+const getRandomValue = (maxValue, minValue = 0) => {
+  return Math.floor(Math.random() * (maxValue - minValue) + minValue);
+};
+
+const generateEntity = () => {
+  const pinValues = {
+    x: getRandomValue(AnnouncerSettings.MAX_X, AnnouncerSettings.MIN_X),
+    y: getRandomValue(AnnouncerSettings.MAX_Y, AnnouncerSettings.MIN_Y),
+  };
+
+  return {
+    author: {
+      avatar: `https://robohash.org/${generateRandomString()}`
+    },
+    offer: {
+      title: AnnouncerSettings.titles[getRandomValue(AnnouncerSettings.titles.length)],
+      address: `${pinValues.x}, ${pinValues.y}`,
+      price: getRandomValue(AnnouncerSettings.MAX_PRICE, AnnouncerSettings.MIN_PRICE),
+      type: AnnouncerSettings.types[getRandomValue(AnnouncerSettings.types.length)],
+      rooms: getRandomValue(AnnouncerSettings.MAX_ROOMS, AnnouncerSettings.MIN_ROOMS),
+      guests: getRandomValue(AnnouncerSettings.MAX_GUESTS, AnnouncerSettings.MIN_GUESTS),
+      checkin: AnnouncerSettings.checkins[getRandomValue(AnnouncerSettings.checkins.length)],
+      checkout: AnnouncerSettings.checkouts[getRandomValue(AnnouncerSettings.checkouts.length)],
+      features: ``,
+      description: ``,
+      photos: ``,
+    },
+    location: {
+      x: pinValues.x,
+      y: pinValues.y,
+    },
+  };
+};
+
+module.exports = {
+  generateEntity,
+  AnnouncerSettings,
+};
---
+++ test/generator/generate-entity.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const {assert} = require(`chai`);
+const {generateEntity, AnnouncerSettings} = require(`../../src/generator/generate-entity`);
+
+const data = generateEntity();
+
+describe(`Generate object of announcement`, () => {
+  it(`should generate object`, () => {
+    assert.isObject(data);
+  });
+
+  describe(`Check author`, () => {
+    it(`should have avatar key`, () => {
+      assert.exists(data.author);
+    });
+    it(`should be correct format of avatar`, () => {
+      assert.isString(data.author.avatar);
+      assert.isNotEmpty(data.author.avatar);
+    });
+  });
+
+  describe(`Check offer`, () => {
+    it(`should have offer key`, () => {
+      assert.exists(data.offer);
+    });
+    it(`should have correct format of title`, () => {
+      for (let i = 0; i < 100; i++) {
+        const testData = generateEntity();
+        assert.include(AnnouncerSettings.titles, testData.offer.title);
+      }
+    });
+    it(`should have correct format of address`, () => {
+      assert.isString(data.offer.address);
+      assert.isNotEmpty(data.offer.address);
+    });
+    it(`should have correct format of locations `, () => {
+      assert.isNumber(data.offer.price);
+    });
+    it(`price should be in correct range`, () => {
+      for (let i = 0; i < 300; i++) {
+        const testData = generateEntity();
+        assert.operator(testData.offer.price, `>=`, AnnouncerSettings.MIN_PRICE);
+        assert.operator(testData.offer.price, `<=`, AnnouncerSettings.MAX_PRICE);
+      }
+    });
+    it(`should have correct format of type`, () => {
+      for (let i = 0; i < 100; i++) {
+        const testData = generateEntity();
+        assert.include(AnnouncerSettings.types, testData.offer.type);
+      }
+    });
+    it(`should generate correct count of rooms `, () => {
+      for (let i = 0; i < 50; i++) {
+        const testData = generateEntity();
+        assert.operator(testData.offer.rooms, `>=`, AnnouncerSettings.MIN_ROOMS);
+        assert.operator(testData.offer.rooms, `<=`, AnnouncerSettings.MAX_ROOMS);
+      }
+    });
+    it(`should generate correct count of guests `, () => {
+      for (let i = 0; i < 50; i++) {
+        const testData = generateEntity();
+        assert.operator(testData.offer.guests, `>=`, AnnouncerSettings.MIN_GUESTS);
+        assert.operator(testData.offer.guests, `<=`, AnnouncerSettings.MAX_GUESTS);
+      }
+    });
+    it(`should have correct format of checkin and checkout`, () => {
+      for (let i = 0; i < 100; i++) {
+        const testData = generateEntity();
+        assert.include(AnnouncerSettings.checkins, testData.offer.checkin);
+        assert.include(AnnouncerSettings.checkouts, testData.offer.checkout);
+      }
+    });
+  });
+
+  describe(`Check location`, () => {
+    it(`should have location key`, () => {
+      assert.exists(data.location);
+    });
+    it(`should have correct format of locations `, () => {
+      assert.isNumber(data.location.x);
+      assert.isNumber(data.location.y);
+    });
+    it(`pin should in correct range `, () => {
+      for (let i = 0; i < 300; i++) {
+        const testData = generateEntity();
+        assert.operator(testData.location.x, `>=`, AnnouncerSettings.MIN_X);
+        assert.operator(testData.location.x, `<=`, AnnouncerSettings.MAX_X);
+        assert.operator(testData.location.y, `>=`, AnnouncerSettings.MIN_Y);
+        assert.operator(testData.location.y, `<=`, AnnouncerSettings.MAX_Y);
+      }
+    });
+  });
+
+});
+
