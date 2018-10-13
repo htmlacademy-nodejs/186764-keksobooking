6058fb6fcd12e6830f21edffc1a65d36435d631d
slava <slava.milin@gmail.com>
2018-10-13 21:04:48 +0300

    refactoring

--- src/generator/generate-entity.js
+++ src/generator/generate-entity.js
@@ -2,13 +2,9 @@
 
 const AnnouncerSettings = require(`./announcer-settings`);
 
-const generateRandomString = () => {
-  return Math.random().toString(36).substring(2);
-};
+const generateRandomString = () => Math.random().toString(36).substring(2);
 
-const getRandomValue = (maxValue, minValue = 0) => {
-  return Math.floor(Math.random() * (maxValue - minValue) + minValue);
-};
+const getRandomValue = (maxValue, minValue = 0) => Math.floor(Math.random() * (maxValue - minValue) + minValue);
 
 const randomShuffle = (getArray) => {
   const array = [...getArray];
