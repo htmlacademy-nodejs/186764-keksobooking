8bed218825e1ded7d705bc78d181bad4a7796050
slava <slava.milin@gmail.com>
2018-11-05 21:55:48 +0300

    fix db

--- src/database/db.js
+++ src/database/db.js
@@ -5,10 +5,11 @@ const {MongoClient} = require(`mongodb`);
 const DB_SETTINGS = {
   URL: `mongodb://localhost:27017`,
   DB_NAME: `keksobooking`
-
 };
 
-module.exports = MongoClient.connect(DB_SETTINGS.url).then((client) => client.db(DB_SETTINGS.DB_NAME)).catch((e) => {
-  console.error(`Failed to connect Mongodb`, e);
-  process.exit(1);
-});
+module.exports = MongoClient.connect(DB_SETTINGS.URL, {useNewUrlParser: true})
+  .then((client) => client.db(DB_SETTINGS.DB_NAME))
+  .catch((e) => {
+    console.error(`Failed to connect Mongodb`, e);
+    process.exit(1);
+  });
